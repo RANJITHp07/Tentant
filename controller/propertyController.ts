1a76b4f02d87efdca6df166e36d03398c4204208
ranjith <ranjithp27122003@gmail.com>
2023-09-29 08:06:02 +0530

    created the filter property

--- controller/propertyController.ts
+++ controller/propertyController.ts
@@ -45,7 +45,7 @@ export const deleteProperty=async(req:Request,res:Response,next:NextFunction)=>{
 // to fetch all the properties
 export const getAllproperties=async(req:Request,res:Response,next:NextFunction)=>{
    try{
-       const getAllproperty=await PropertyModel.find().populate("userId");
+       const getAllproperty=await PropertyModel.find().populate("userId").sort({createdAt:-1});
        res.status(200).json({success:true,message:"Got all properties",data:getAllproperty})
    }catch(err){
         new Error(`Property err:${err}`);
@@ -64,4 +64,35 @@ export const getSingleUserProperties=async(req:Request,res:Response,next:NextFun
         next(err)
     }
 }   
+
+export const filterProperties=async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+         const {
+           city,
+           date,
+           price,
+           type
+         }=req.query
+
+         const filter:any=[]
+         if(city){
+            filter.push({ property_address: { $regex: city, $options: 'i' } });
+         }else if(date){
+            filter.push({available_date:{$in:date}})
+         }else if(price){
+            filter.push({ price: { $gt: Number(price) } });
+         }else if(type){
+            filter.push({type:type})
+         }
+
+         const properties=await PropertyModel.find(filter)
+         await PropertyModel.find({
+            $and: filter.length > 0 ? filter : [{}]
+          }).populate('userId').sort({createdAt:-1});
+
+          res.status(200).json({success: true,message:"Filtered properties",data:properties});
+    }catch(err){
+        throw err
+    }
+}
 
